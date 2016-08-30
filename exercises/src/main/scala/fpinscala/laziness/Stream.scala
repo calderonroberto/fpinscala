7249b0cc973b3a8082145884b96945a9b34debf8
Roberto Calderon <roberto@robertocalderon.ca>
2016-08-29 21:35:05 -0700

    streams

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,13 +3,27 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
+  def toList: List[A] = this match {
+    case Cons(h, t) => h() :: t().toList
+    case _ => List()
+  }
+
+  final def toListTailRec: List[A] = { //had a peeek at the solution, because i didn't get why it was not compiling
+    @annotation.tailrec
+    def makeList(l: List[A], s: Stream[A]): List[A] = s match {
+        case Cons(h, t) => makeList(h() :: l, t())
+        case _ => l
+      }
+    makeList(List(), this).reverse
+  }
+
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =   //TODO: I don't get this explanation, how does 'b' fold the tail? how does scala know to run fold on it?
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -17,20 +31,72 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = this match  {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => cons(h(), t().drop(n-1))
+    case _ => empty
+  }
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+  @annotation.tailrec
+  final def dropTailRec(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().dropTailRec(n - 1)
+    case _ => this
+  }
 
-  def headOption: Option[A] = sys.error("todo")
+  final def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h,t) if p(h()) => cons(h(), t().takeWhile(p)) // meh, forgot to  create a new cons(), instead i was passing only the tail
+    case _ => Empty
+  }
+
+  def forAll(p: A => Boolean): Boolean = foldRight(true)((h,t) => p(h) && t)
+
+  def takeWhileFoldRight(p: A => Boolean): Stream[A] = foldRight(empty[A])((h,t) => if (p(h)) cons(h,t) else empty[A] ) // TODO: i kind of get it, but i'm still missing why cons(h,t), 't' will call fold right
+
+  def headOption: Option[A] = foldRight(None: Option[A])((h,t) => Some(h)) //TODO:  is Empty[A] == None: Option[A]?
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
+  def map[B](f: A => B): Stream[B] = foldRight(empty[B])((h,t) => cons(f(h), t)) //TODO: is empty[B] == Empty: Stream[B]?
+
+  def filter(f: A => Boolean): Stream[A] = foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else t)
+
+  //append adds the tail to
+  def append[B>:A](f: => Stream[B]): Stream[B] = foldRight(f)((h,t) => cons(h,t)) //TODO: seriously, how does t call append again?
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(empty[B])((h,t) => f(h) append t)
+
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+  def constant2[A](a: A): Stream[A] = {
+    lazy val tail: Stream[A] = Cons(() => a, () => tail)
+    tail
+  }
+
+  def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+  def fib: Stream[Int] = {
+    def go(a: Int, b: Int): Stream[Int] = {
+      cons(a, go(a, a+b))
+    }
+    go(0,1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z) match {
+      case Some((a,b)) => cons(a, unfold(b)(f)) // this ((a,b)) threw me off, it's ONE parameter, not two
+      case _ => empty
+    }
+  }
+
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -45,11 +111,20 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
   def from(n: Int): Stream[Int] = sys.error("todo")
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+}
+
+object Chapter5 {
+  def main(args: Array[String]): Unit = {
+    val s = Stream(1,2,3)
+    println(s.toListTailRec)
+    println(s.constant(1).take(4).toList)
+    println(s.from(100).toList)
+  }
 }
