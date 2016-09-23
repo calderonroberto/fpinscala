a64ad58ced503f98e8e16517120202ef45e42354
Roberto Calderon <roberto@robertocalderon.ca>
2016-09-22 17:37:54 -0700

    map and map2 in terms of flatmap

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -119,6 +119,23 @@ object RNG {
     }
   }
 
+  def mapInTermsOfFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s) { a =>
+      unit(f(a))
+    }
+
+  def map2InTermsOfFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra) { a =>
+      flatMap(rb) { b =>
+        unit(f(a, b))
+      }
+    } // TODO: Discuss if this will this work?
+
+  def map2InTermsOfFlatMap2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra) { a =>
+      map(rb)(b => f(a, b))
+    }
+
 }
 
 case class State[S, +A](run: S => (A, S)) {
